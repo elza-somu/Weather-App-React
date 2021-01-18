91c23b4dcf889d82dac63efdebdf739b4892dd50
elza-s <elza_somu@yahoo.com>
2021-01-18 09:03:22 -0500

    changed styling

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Footer.css"
 
 export default function Footer() {
   return (
     <div className="Footer">
       <p class="openSource">
         <a href="https://github.com/elza-s/Weather-App-React" target="_blank" rel="noreferrer">Open source code </a>
-          by Elza Somu
+          <span> by Elza Somu </span>
       </p>
     </div>
   );
